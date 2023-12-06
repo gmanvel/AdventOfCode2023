65aa15e5ac758d778f71c16ffc8ca5e4f8ce1f04
Manvel Ghazaryan <manvel.ghazaryan@alfalaval.com>
2023-12-06 15:50:13 +0100

    December 6 Task 1/2

---
+++ December 6/December6.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode2023.December_6;
+
+public class December6 : IPuzzle
+{
+    public void SolvePart1(object input)
+    {
+        if (input is not List<(long Time, long Distance)> pairs)
+            throw new ArgumentException(nameof(input));
+
+        Console.WriteLine(
+            pairs
+                .Select(timeDistancePairs =>
+                {
+                    long lastSeenDistance = 0;
+                    HashSet<long> matches = new();
+                    foreach (var number in Enumerable.Range(1, (int)timeDistancePairs.Time - 1))
+                    {
+                        var distance = (timeDistancePairs.Time - number) * number;
+
+                        if (distance == lastSeenDistance)
+                            return matches.Count * 2;
+
+                        if (distance > timeDistancePairs.Distance)
+                        {
+                            lastSeenDistance = distance;
+                            matches.Add(number);
+                        }
+                    }
+
+                    return matches.Count;
+                })
+                .Aggregate((total, next) => total * next));
+    }
+
+    public void SolvePart2(object input)
+    {
+        if (input is not List<(long Time, long Distance)> pairs)
+            throw new ArgumentException(nameof(input));
+
+        Console.WriteLine(
+            pairs
+                .Select(timeDistancePairs =>
+                {
+                    long lastSeenDistance = 0;
+                    HashSet<long> matches = new();
+                    foreach (var number in Enumerable.Range(1, (int)timeDistancePairs.Time - 1))
+                    {
+                        var distance = (timeDistancePairs.Time - number) * number;
+
+                        if (distance == lastSeenDistance)
+                            return matches.Count * 2;
+
+                        if (distance > timeDistancePairs.Distance)
+                        {
+                            lastSeenDistance = distance;
+                            matches.Add(number);
+                        }
+                    }
+
+                    return matches.Count;
+                })
+                .Sum());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using AdventOfCode2023;
-using AdventOfCode2023.December_4;
+using AdventOfCode2023.December_6;
 
-var input = File.ReadAllLines(
-    Path.Combine(
-        AppDomain.CurrentDomain.BaseDirectory,
-        "December 4",
-        "Dec4_Input.txt"));
-
-IPuzzle puzzle = new December4();
-puzzle.SolvePart1(input);
-puzzle.SolvePart2(input);
+IPuzzle puzzle = new December6();
+puzzle.SolvePart1(new List<(long Time, long Distance)> { (59, 597), (79, 1234), (65, 1032), (75, 1328) });
+puzzle.SolvePart2(new List<(long Time, long Distance)> { (59796575, 597123410321328) });
